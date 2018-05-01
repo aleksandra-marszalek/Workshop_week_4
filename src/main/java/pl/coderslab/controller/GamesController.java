55d8e97c14da81ad2d2106f09db8f0282877f848
Aleksandra Marszalek <aaleksandra.mmarszalek@gmail.com>
2018-05-01 21:55:49 +0200

    Update controller and faker resource

--- src/main/java/pl/coderslab/controller/GamesController.java
+++ src/main/java/pl/coderslab/controller/GamesController.java
@@ -1,15 +1,20 @@
 package pl.coderslab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.coderslab.services.FakerService;
 
+@Controller
 public class GamesController {
 
     @Autowired
     FakerService fakerService;
 
-    @GetMapping(path= "/fake-today-games") public String sample() {
+    @GetMapping("/fake-today-games")
+    @ResponseBody
+    public String sample() {
         return fakerService.getTodayGames().toString();
     }
 }
--- src/main/java/pl/coderslab/web/rest/FakerResource.java
+++ src/main/java/pl/coderslab/web/rest/FakerResource.java
@@ -3,6 +3,7 @@ package pl.coderslab.web.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class FakerResource {
-    @GetMapping(path= "/hello-world") public String helloWorld() {
+    @GetMapping(path= "/hello-world")
+    public String helloWorld() {
         return "Hello World"; }
 }
