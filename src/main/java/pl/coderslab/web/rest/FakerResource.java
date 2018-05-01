85f8568b9bd433d394b64b6939c3c8be9c7cca10
Aleksandra Marszalek <aaleksandra.mmarszalek@gmail.com>
2018-05-01 22:13:09 +0200

    Update classes according to presentation

--- src/main/java/pl/coderslab/web/rest/FakerResource.java
+++ src/main/java/pl/coderslab/web/rest/FakerResource.java
@@ -1,9 +1,28 @@
 package pl.coderslab.web.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.services.FakerService;
 
+
+@RestController
+@RequestMapping("/api")
 public class FakerResource {
-    @GetMapping(path= "/hello-world")
+
+    @GetMapping("/hello-world")
     public String helloWorld() {
-        return "Hello World"; }
+        return "Hello World";
+    }
+
+    @Autowired
+    FakerService fakerService;
+
+    @GetMapping("/fake-today-games")
+    @ResponseBody
+    public String sample() {
+        return fakerService.getTodayGames().toString();
+    }
 }
